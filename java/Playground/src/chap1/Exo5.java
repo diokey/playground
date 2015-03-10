1656a0c5372a4efd1fa6fb2985f562355d65385d
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-03-10 00:20:30 -0400

    Improved compression algorithm using string buffer

--- java/Playground/src/chap1/Exo5.java
+++ java/Playground/src/chap1/Exo5.java
@@ -20,7 +20,32 @@ public class Exo5 {
 		return compressed + prev + counter;
 	}
 	
+	/*
+	 * The second solution introduces a small optimization using string buffer. 
+	 * This allows the algorithm to run in O(n) space and O(n) time
+	 */
+	public static String compress2(String word) {
+		int l = word.length();
+		StringBuilder compressed = new StringBuilder();
+		char prev = word.charAt(0);
+		int counter = 1;
+		for(int i=1; i<l; i++) {
+			
+			if(word.charAt(i) == prev) {
+				counter++;
+			} else {
+				compressed.append(prev).append(counter);
+				prev = word.charAt(i);
+				counter = 1;
+			}
+		}
+		String res = compressed.append(prev).append(counter).toString();
+		return res.length() > word.length() ? 
+				word : compressed.append(prev).append(counter).toString();
+	}
+	
 	public static void main(String args[]) {
-		System.out.println(compress("aabcccccaaa"));
+		System.out.println(compress2("aabcccccaaa"));
+		System.out.println(compress2("abcdefge"));
 	}
 }
