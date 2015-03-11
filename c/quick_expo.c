413207e0c48cac421f2f687bb8458f49993c46b7
Olivier Dusabimana <diokeyolivier@gmail.com>
2015-03-10 23:34:13 -0400

    Explain alogirthm used

--- c/quick_expo.c
+++ c/quick_expo.c
@@ -1,6 +1,13 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+
+/*
+ * This algorithm uses recursion to compute
+ * the value of an exponent. This allows This
+ * algorithm to run with a complexity of O(logn)
+ * as opposed to O(n) otherwise
+ */
 int quick_expo(int x, int n) {
   int tmp = 0;
   if (n == 0)
